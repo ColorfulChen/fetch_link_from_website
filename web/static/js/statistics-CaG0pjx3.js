c7ad0848e9aca50aed7ec4dbc9726622c2601357
gjssss <gaojinsong2010@gmail.com>
2025-10-27 03:40:28 +0000

    fix: fix mock login api

---
+++ web/static/js/statistics-CaG0pjx3.js
@@ -0,0 +1,1 @@
+import{az as s}from"./index-Bt5xf_d8.js";const r=t=>s.request("get","/statistics",{params:t});export{r as g};
--- web/static/js/statistics-DqXhqvTb.js
+++
@@ -1,1 +0,0 @@
-import{az as s}from"./index-Cghf-nif.js";const r=t=>s.request("get","/statistics",{params:t});export{r as g};
